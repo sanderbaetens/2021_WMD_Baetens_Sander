35103ac4869b1b58a29dfd44133b0b951d372ebd
sanderbaetens <46609425+sanderbaetens@users.noreply.github.com>
2020-10-20 18:33:23 +0200

    Add files via upload

---
+++ Oefening 8/Oefening 8.1/oefening8.1.js
@@ -0,0 +1,23 @@
+/* eslint-disable require-jsdoc */
+
+(function() {
+  const lettergrepen = ['ama', 'reg', 'as', 'yt', 'tot', 'dip', 'kul', 'ko', 'uli', 'aze', 'ifu', 'mom', 'vel', 'ema', 'go', 'um', 'se', 'tyl', 'qu', 'fin', 'sij', 'ug', 'muk', 'se', 'fer', 'wil'];
+
+  const klas = [];
+
+  function lettergreep() {
+    return lettergrepen[Math.floor(Math.random() * lettergrepen.length)];
+  }
+
+
+  for (let i = 1; i < 23; i++) {
+    const student = {
+      nummer: 'NR-'+ i,
+      voornaam: lettergreep() + lettergreep() + '',
+      achternaam: lettergreep() + lettergreep() + lettergreep(),
+    };
+    klas.push(student);
+  }
+  console.table(klas);
+})();
+
